65cb0e86e0e3006865b9151dcf9b8fe0e1394919
hirofumi.ogawa <hirofumi.ogawa@FO-1277.local>
2025-12-05 00:35:42 +0900

    Taskのstatus変更

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,7 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+// app/javascript/controllers/index.js
+import { Application } from "@hotwired/stimulus"
+import SidePanelController from "./side_panel_controller"
+
+const application = Application.start()
+
+application.register("side-panel", SidePanelController)
